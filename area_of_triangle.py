ea8f92280d08b48a5e31f71a164110101f60b2ae
Aditya Kumar Shaw <112172428+GODofGOD007@users.noreply.github.com>
2023-10-07 22:09:52 +0530

    Create area_of_triangle.py

---
+++ area_of_triangle.py
@@ -0,0 +1,4 @@
+h = int(input("\n\nEnter the height of the Triangle: "))
+b = int(input("\n\nEnter the base of the Triangle: "))
+area = (h * b) / 2
+print("\n\n\nThe area of the triangle is:", area)
