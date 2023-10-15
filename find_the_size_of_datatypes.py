5daf35da5a46f20a8dae461b560a50a5e29cc9a2
Aditya Kumar Shaw <112172428+aditya-githb@users.noreply.github.com>
2023-10-15 20:03:43 +0530

    Create find_the_size_of_datatypes.py

---
+++ find_the_size_of_datatypes.py
@@ -0,0 +1,8 @@
+intType = 0
+floatType = 0.0
+doubleType = 0.0
+charType = ''
+print("Size of int:", intType.__sizeof__(), "bytes")
+print("Size of float:", floatType.__sizeof__(), "bytes")
+print("Size of double:", doubleType.__sizeof__(), "bytes")
+print("Size of char:", charType.__sizeof__(), "byte")
